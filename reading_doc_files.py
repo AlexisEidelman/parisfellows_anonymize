134b6dac2e6c1e0a229c3009afa4778262f62302
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2016-12-15 10:27:44 +0100

    reoarganisation reading doc (WIP)

--- reading_doc_files.py
+++ reading_doc_files.py
@@ -15,8 +15,6 @@ from nltk.tokenize import word_tokenize
 from sklearn.preprocessing import LabelEncoder
 
 
-
-
 stopword_fr = [word for word in stopwords.words('french')]
 
 target_dict = {u'X\xe2\x80\xa6' : 'X', u'X' : 'X', u'X..' : 'X', u'X.' : 'X',
@@ -25,8 +23,6 @@ target_dict = {u'X\xe2\x80\xa6' : 'X', u'X' : 'X', u'X..' : 'X', u'X.' : 'X',
                u'Y\u2026' : 'Y', u'M.Y' : 'Y', u'M.Y\u2026' : 'Y',
                u'Z\xe2\x80\xa6': 'Z', u'Z' : 'Z', u'Z..' : 'Z', u'Z.' : 'Z',
                u'Z\u2026' : 'Z', u'M.Z' : 'Z', u'M.Z\u2026' : 'Z'}
-
-mister_list = [u'M', u'M.', u'Madame', u'Mme', u'Monsieur', u'Dr', u'Monsieur', u'MM']
 
 #punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 
@@ -81,48 +77,50 @@ jurinet_files = [x for x in os.listdir(path_data) if 'jurinet' in x]
 path_files = [os.path.join(path_data, f) for f in jurinet_files]
 
 
-#    with open(dila_file, 'r') as dila:
-#        dila_text = dila.read()
-#
-#    dila_text = re.sub(r'([A-Z])\.\.\.', r'\1zzz', dila_text)
-##    text1bis = text1.replace('...', 'zzz')
-#    dila_text = re.sub(r'zzz-\w', r'', dila_text)
-#    mots_dila = word_tokenize(dila_text, language='french')
-#    mots_juri = word_tokenize(jurinet_text, language='french')
-#
-#    for k in range(len(mots_dila)):
-#        if mots_dila[k] != mots_juri[k]:
-#            print(mots_dila[k], mots_juri[k])
+def df_from_all_docs(path_files):
+    ''' 
+    retourne un df avec tous les mots de chaque document de la 
+    liste path_files
+        - les mots sont entendu au sens word_tokenize
+        - le dataframe présente sur chaque ligne :
+            - le mot (dans le texte jurinet)
+            - le mot (dans le texte dila même si c'est inutile)
+            - est-ce que ce mot est "tagged" ou non
+            - le nom du document auquel appartient le doc
+    '''
 
-words_df = pd.DataFrame()
-document_temp = pd.DataFrame()
-
-
-for path_file in path_files:
-    num_file = os.path.basename(path_file)[:-4].split('_')[0]
-    print ("Loading file : " + os.path.basename(path_file))
+    words_df = pd.DataFrame()
+    document_temp = pd.DataFrame()
     
-    with open(path_file, 'r') as jurinet:
-        jurinet_text = jurinet.read()
-    with open(path_file.replace('_jurinet', '_dila'), 'r') as dila:
-        dila_text = dila.read()
+    for path_file in path_files:
+        num_file = os.path.basename(path_file)[:-4].split('_')[0]
+        print ("Loading file : " + os.path.basename(path_file))
         
-    mots_init = word_tokenize(jurinet_text, language='french')
-    text_modifie = re.sub(r'([A-Z])\.\.\.', r'\1zzz', dila_text)
-    text_modifie = re.sub(r'zzz-\w', r'', text_modifie)
-    mots_modif = word_tokenize(text_modifie, language='french')
+        with open(path_file, 'r') as jurinet:
+            jurinet_text = jurinet.read()
+        with open(path_file.replace('_jurinet', '_dila'), 'r') as dila:
+            dila_text = dila.read()
+            
+        mots_init = word_tokenize(jurinet_text, language='french')
+        text_modifie = re.sub(r'([A-Z])\.\.\.', r'\1zzz', dila_text)
+        text_modifie = re.sub(r'zzz-\w', r'', text_modifie)
+        mots_modif = word_tokenize(text_modifie, language='french')
+        
+        tagged = noms_retires(mots_init, mots_modif, verbose=False)
+        
+        document_temp = pd.DataFrame.from_dict({
+        'mot': mots_init,
+        'tagged': tagged,    
+        })
+        document_temp['doc_name'] = num_file
+        document_temp['rank_word'] = range(len(document_temp))
+        
+        words_df = pd.concat([words_df, document_temp])
     
-    tagged = noms_retires(mots_init, mots_modif, verbose=False)
-    
-    document_temp = pd.DataFrame.from_dict({
-    'mot': mots_init,
-    'tagged': tagged,    
-    })
-    document_temp['doc_name'] = num_file
-    document_temp['rank_word'] = range(len(document_temp))
-    
-    words_df = pd.concat([words_df, document_temp])
+    return words_df
 
+
+words_df = df_from_all_docs(path_files)
 print(words_df.groupby('doc_name')['tagged'].sum())
 print(words_df.groupby('doc_name')['tagged'].sum().value_counts())
 
@@ -140,60 +138,79 @@ print(" PREPROCESSING...")
 
 
 
+def read_name_list():
+    ## Loading annexe files (Firstaname / Names etc;..)
+    # Reading French's firstnames file
+    firstname_df = pd.read_csv('other/data/prenom_clean.csv', encoding='utf-8',
+                               header=None, names = ["firstname"])
+    # Use Majuscule in first carac
+    firstname_df['firstname'] = firstname_df['firstname'].str.title()
+    firstname_list = firstname_df.firstname.tolist()
+    
+    # Reading foreign's firstnames file
+    foreign_firstname_df =  pd.read_csv('other/data/foreign_fistname_clean.csv', encoding='utf-8',
+                                 header=None, names = ["firstname"])
+    foreign_firstname_list = foreign_firstname_df.firstname.tolist()
+    
+    # Name list
+    name_df = pd.read_csv('other/data/top_8k_name.csv', encoding='utf-8')
+    name_list = name_df.name.tolist()
+    return firstname_list, foreign_firstname_list, name_list
 
-## Loading annexe files (Firstaname / Names etc;..)
-# Reading French's firstnames file
-firstname_df = pd.read_csv('other/data/prenom_clean.csv', encoding='utf-8',
-                           header=None, names = ["firstname"])
-# Use Majuscule in first carac
-firstname_df['firstname'] = firstname_df['firstname'].str.title()
-firstname_list = firstname_df.firstname.tolist()
+firstname_list, foreign_firstname_list, name_list = read_name_list()
 
-# Reading foreign's firstnames file
-foreign_firstname_df =  pd.read_csv('other/data/foreign_fistname_clean.csv', encoding='utf-8',
-                             header=None, names = ["firstname"])
-foreign_firstname_list = foreign_firstname_df.firstname.tolist()
-
-# Name list
-name_df = pd.read_csv('other/data/top_8k_name.csv', encoding='utf-8')
-name_list = name_df.name.tolist()
+mister_list = [u'M', u'M.', u'Madame', u'Mme', u'Monsieur', u'Dr', u'Monsieur', u'MM']
 
 
-##################################################
-####             Caractéristique du mot        ###
+def caracterisqute_du_mot(words_df, 
+                          stopword_df,
+                          firstname_list,
+                          foreign_firstname_list,
+                          mister_list):
 
-# Check if word is Firstname then is_target :
+    # nombre occurence dans le doc
+    count_mot = words_df.groupby(['doc_name', 'mot']).size().to_frame('nb_mot').reset_index()
+    words_df = words_df.merge(count_mot, on = ['doc_name', 'mot'])
+    
+    words_df['is_stopword'] = words_df['mot'].str.lower().isin(stopword_fr)
+    words_df['is_first_char_upper'] = words_df['mot'].str[0].str.isupper()
+    words_df['is_upper'] = words_df['mot'].str.isupper()
+    words_df['len_word'] = words_df['mot'].str.len()
+    words_df['is_firstname'] = words_df['mot'].isin(firstname_list)
+    words_df['is_foreign_firstname'] = words_df['mot'].isin(foreign_firstname_list)
+    words_df['is_mister_word'] = words_df['mot'].isin(mister_list)
+    # id encoding usefull ? 
+    lbl = LabelEncoder()
+    words_df['word_encoded'] = lbl.fit_transform(list(words_df['mot'].values))
+    return words_df
 
-words_df['is_stopword'] = words_df['mot'].str.lower().isin(stopword_fr)
-words_df['is_first_char_upper'] = words_df['mot'].str[0].str.isupper()
-words_df['is_upper'] = words_df['mot'].str.isupper()
-words_df['len_word'] = words_df['mot'].str.len()
-words_df['is_firstname'] = words_df['mot'].isin(firstname_list)
-words_df['is_foreign_firstname'] = words_df['mot'].isin(foreign_firstname_list)
-words_df['is_mister_word'] = words_df['mot'].isin(mister_list)
-#words_df['is_firstname'] = words_df['mot'].str.title().isin(firstname_list)
-## Label encoding word
-lbl = LabelEncoder()
-words_df['word_encoded'] = lbl.fit_transform(list(words_df['mot'].values))
 
-caracteristique_mot = ['mot', 'is_firstname', 'is_stopword', 'is_first_char_upper',
+
+words_df = words_df(caracterisqute_du_mot)
+
+
+def shift_words_data(words_df,
+                     nb_mot_avant, nb_mot_apres,
+                     liste_caracteristiques):
+    ''' update words_df table (by doc_name'''
+    for k in range(nb_mot_avant, nb_mot_apres):
+        liste_caracteristiques_k = [nom + ' ' + str(k) for nom in liste_caracteristiques]
+        if k  != 0:
+            words_df[liste_caracteristiques_k] = \
+                words_df.groupby(['doc_name'])[liste_caracteristiques].apply(lambda x: x.shift(k))
+
+caracteristiques_mot = ['mot', 'is_firstname', 'is_stopword', 'is_first_char_upper',
                        'is_upper', 'len_word', 'is_mister_word', 'word_encoded',
-                       'is_foreign_firstname']
-for k in range(-4, 5):
-    caracteristique_mot_k = [nom + ' ' + str(k) for nom in caracteristique_mot]
-    if k  != 0:
-        words_df[caracteristique_mot_k] = \
-            words_df.groupby(['doc_name'])[caracteristique_mot].apply(lambda x: x.shift(k))
-# TODO: peut-être une petite optimisation pour le firstname on peut aller chercher la valuer 
+                       'is_foreign_firstname',
+                       'nb_mot']
 
-# nombre occurence dans le doc
-count_mot = words_df.groupby(['doc_name', 'mot']).size().to_frame('nb_mot').reset_index()
-words_df = words_df.merge(count_mot, on = ['doc_name', 'mot'])
+words_df = shift_words_data(words_df, -4, 5, caracteristiques_mot)
 
 
 
 ##########################################################
-####             Caractéristique de la postiion        ###
+####             Caractéristique de la position        ###
+
 
 # to have granularite
 words_df['temp_count'] = 1
@@ -227,7 +244,7 @@ words_df = words_df.drop(['temp_count', 'end_comma', 'end_comma_cum',
 # TODO: entre is_entre_guillemet
 
 # Fillna Nan word shift
-words_df = words_df.fillna(-1)
+#words_df = words_df.fillna(-1)
 
 #print " EXPORT..."
 words_df.to_csv('data/data.csv', encoding='utf-8', index=False)
